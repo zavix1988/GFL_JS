05aac8d51eac902268893f165e60cb1b3885c56e
Zhukov <zavix1988@gmail.com>
2019-05-20 10:18:54 +0300

    final js

--- HomeWork/Angular/shop/src/app/services/share.service.ts
+++ HomeWork/Angular/shop/src/app/services/share.service.ts
@@ -5,42 +5,42 @@ export class ShareService {
   productsArr = [
     {
       id: 1,
-      name: 'milk',
-      price: 100,
+      name: 'esp32',
+      price: 5,
       count: 0,
     },
     {
       id: 2,
-      name: 'bread',
-      price: 200,
+      name: 'esp8266',
+      price: 2,
       count: 0,
     },
     {
       id: 3,
-      name: 'oil',
-      price: 300,
+      name: 'arduino nano',
+      price: 4,
       count: 0,
     },
     {
       id: 4,
-      name: 'apple',
-      price: 400,
+      name: 'Raspberry Pi3',
+      price: 50,
       count: 0,
     },
     {
       id: 5,
-      name: 'carrot',
-      price: 500,
+      name: 'OrangePi + 2e',
+      price: 54,
       count: 0,
     },
     {
       id: 6,
-      name: 'banana',
-      price: 600,
+      name: 'Heltec LoRa 32',
+      price: 7,
       count: 0,
     },
   ];
-  products = JSON.parse(localStorage.shopCw14 || JSON.stringify(this.productsArr));
+  products = JSON.parse(localStorage.ngShop || JSON.stringify(this.productsArr));
   
   constructor() {
     
--- HomeWork/Vue/calendar/js/script.js
+++ HomeWork/Vue/calendar/js/script.js
@@ -1,76 +1,136 @@
 Vue.component('calendar', {
     template: `
-    <table>
-        <thead>
-            <tr>
-                {{ setDefaultVals }}
-                <td><button @click="prevMonth">‹</button></td>
-                <td colspan="5">{{ monthYear }}</td>
-                <td><button @click="nextMonth">›</button></td>
-            </tr>
-            <tr v-if="beginWeekMonday">
-                <th>Mon</th>
-                <th>Tue</th>
-                <th>Wed</th>
-                <th>Thu</th>
-                <th>Fri</th>
-                <th>Sat</th>
-                <th>Sun</th>
-            </tr>
-            <tr v-else>
-                <th>Sun</th>
-                <th>Mon</th>
-                <th>Tue</th>
-                <th>Wed</th>
-                <th>Thu</th>
-                <th>Fri</th>
-                <th>Sat</th>
-            </tr>            
-        </thead>
-        <tbody>
-            <tr>
-                <td v-for="day in daysInMonth">{{ day }}</td>
-            </tr>
-        </tbody>
-    </table>
-    `,
-    data() {
+                        <table id="calendar2">
+                        <thead>
+                            <tr>
+                                {{ setDefaultVals }}
+                                <button @click="prevMonth">‹</button>
+                                <td colspan="5">{{ monthYear }}</td>
+                                <button @click="nextMonth">›</button>
+                            </tr>
+                            <tr>
+                                <td>Mon</td>
+                                <td>Tue</td>
+                                <td>Wed</td>
+                                <td>Thu</td>
+                                <td>Fri</td>
+                                <td>Sat</td>
+                                <td>Sun</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="week in days">
+                                <td v-for="day in week">{{ day.dayNum }}</td>
+                            </tr>
+                        </tbody>
+                    </table>`,
+    data: function () {
         return {
-            date: new Date,
+            date: new Date(),
             monthes: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'October', 'December'],
             selectedMonth: undefined,
             selectedYear: undefined,
             lastDay: undefined,
-            beginWeekMonday: false,
-            daysInMonth: undefined,
-
+            firstWeekDay: undefined,
+            lastWeekDay: undefined,
+            days: []
         }
     },
     methods: {
-        getCurrentMonth: function () {
+        currentMonth: function () {
             return this.selectedMonth = this.date.getMonth();
         },
-        getCurrentYear: function () {
+        currentYear: function () {
             return this.selectedYear = this.date.getFullYear();
         },
-        getDaysInMonth: function() {
-           return this.daysInMonth =  33 - new Date(this.selectedYear, this.selectedMonth, 33).getDate()
-        }
+        showMonth: function (month) {
+            this.selectedMonth = month;
+            return this.monthes[month];
+        },
+        nextMonth: function () {
+            this.getFirstWeekDay();
+            this.getLastDay();
+            this.getLastWeekDay();
+            this.getMonthesDays();
+            if (this.selectedMonth < 11) {
+                return this.selectedMonth++;
+            }
+            this.selectedYear++;
+            return this.selectedMonth = 0;
+        },
+        prevMonth() {
+            this.getFirstWeekDay();
+            this.getLastDay();
+            this.getLastWeekDay();
+            this.getMonthesDays();
+            if (this.selectedMonth > 0) {
+                return this.selectedMonth--;
+            }
+            this.selectedYear--;
+            return this.selectedMonth = 11;
+        },
+        getLastDay: function () {
+            this.lastDay = new Date(this.selectedYear, this.selectedMonth + 1, 0).getDate()
+        },
+        getFirstWeekDay: function () {
+            this.firstWeekDay = new Date(this.selectedYear, this.selectedMonth, 1).getDay();
+        },
+        getLastWeekDay: function () {
+            this.getLastDay();
+            this.lastWeekDay = new Date(this.selectedYear, this.selectedMonth, this.lastDay).getDay()
+        },
+        getMonthesDays: function () {
+            var week = 0;
+            this.days[week] = [];
+            for (var i = 1; i <= this.lastDay; i++) {
+                if (new Date(this.selectedYear, this.selectedMonth, i).getDay() != 1) {
+                    this.days[week].push({
+                        dayNum: i,
+                        dayWeek: new Date(this.selectedYear, this.selectedMonth, i).getDay()
+                    })
+                } else {
+                    week++;
+                    if (!this.days[week]) {
+                        this.days[week] = [];
+                    }
+                    this.days[week].push({
+                        dayNum: i,
+                        dayWeek: new Date(this.selectedYear, this.selectedMonth, i).getDay()
+                    })
+                }
+            }
 
-    },
-    created: function() {
-        this.getCurrentMonth();
-        this.getCurrentYear();   
-        this.getDaysInMonth();   
+        },
+        formatFirstWeek: function () {
+            //console.log(this.days[0].length)
+            for (var i = this.days[0].length; i < 7; i++) {
+                this.days[0].unshift({
+                    dayNum: undefined,
+                    dayWeek: i
+                });
+            }
+        }
     },
     computed: {
-        monthYear(){
-            return this.monthes[this.date.getMonth()];
+        setDefaultVals: function () {
+            this.getFirstWeekDay();
+            this.getLastWeekDay();
+            this.getLastDay();
+            this.getMonthesDays();
+            this.formatFirstWeek();
+        },
+        monthYear: function () {
+            this.days = [];
+            if (void 0 === this.selectedMonth) {
+                this.selectedMonth = this.currentMonth();
+            }
+            if (void 0 === this.selectedYear) {
+                this.selectedYear = this.currentYear();
+            }
+            return this.monthes[this.selectedMonth] + " " + this.selectedYear;
         }
     }
 });
-
-
 var app = new Vue({
     el: '#app'
 })
